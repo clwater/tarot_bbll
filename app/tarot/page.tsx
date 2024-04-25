209a5ab720e75387c555197557d4c9a32ed552ac
clwater <gzb@clwater.top>
2024-04-25 16:44:44 +0800

    wip: loop

--- app/tarot/page.tsx
+++ app/tarot/page.tsx
@@ -1,14 +1,22 @@
-import Image from "next/image";
-import {Button, ButtonGroup} from "@nextui-org/react";
 import {HeaderT} from "@/app/componets/header";
+import React from "react";
 export default function Tarot() {
-  return (
+
+    const array = Array(20).fill(undefined)
+
+
+    return (
         <div>
             <HeaderT />
-            <Button color="primary">
-                Button
-            </Button>
-            Tarot
+            <div>
+                <ul>
+                    {array.map((_, index) => (
+                        <li key={index}>
+                            {index}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
 
   );
