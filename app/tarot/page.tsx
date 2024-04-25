9b39ba5fbb4535c1c8cf7ef70c6af25c09dccda3
clwater <gzb@clwater.top>
2024-04-25 16:21:31 +0800

    wip: route

--- app/tarot/page.tsx
+++ app/tarot/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import {Button, ButtonGroup} from "@nextui-org/react";
 import {HeaderT} from "@/app/componets/header";
-export default function Home() {
+export default function Tarot() {
   return (
         <div>
             <HeaderT />
             <Button color="primary">
                 Button
             </Button>
+            Tarot
         </div>
 
   );
