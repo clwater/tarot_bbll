466de8a88f7aec16aef7dbcd8d708dff46cc2c78
clwater <gzb@clwater.top>
2024-04-25 15:25:02 +0800

    wip: theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,36 @@
 import {nextui} from '@nextui-org/theme';
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx},\r\n    ./components/**/*.{js,ts,jsx,tsx,mdx},\r\n    ./app/**/*.{js,ts,jsx,tsx,mdx},",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx},\r\n    ./components/**/*.{js,ts,jsx,tsx,mdx},\r\n    ./app/**/*.{js,ts,jsx,tsx,mdx},",
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+        },
     },
-  },
-  darkMode: "class",
-  plugins: [nextui()],
+    darkMode: "class",
+    plugins: [
+        nextui({
+            themes: {
+                light: {
+                    colors: {
+                        primary: "#9353d3",
+                    }
+                },
+                dark: {
+                    colors: {
+                        primary: "#9353d3",
+                    }
+                },
+            },
+        }),
+    ]
 };
 export default config;
