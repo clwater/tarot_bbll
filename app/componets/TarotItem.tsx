93b35040cb38a127c38137fd444a0f5bbc6a2158
clwater <gzb@clwater.top>
2024-04-26 13:13:36 +0800

    wip: tarot item

--- app/componets/TarotItem.tsx
+++ app/componets/TarotItem.tsx
@@ -4,12 +4,12 @@ import {Header} from "@/app/componets/header";
 
 let TarotManager = require('@/app/utils/TarotManager');
 
-export function TarotItem() {
+export async function TarotItem() {
     const searchParams = useSearchParams()
 
     let id = searchParams.get('id');
     if (!id) return id = '0';
-    const card = TarotManager.getTarotData(parseInt(id));
+    const card = await TarotManager.getTarotData(parseInt(id));
     return (
         <Suspense>
             <div>
