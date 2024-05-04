d8b3d38687e59b72afdb5fe63ee75cb4026572ba
clwater <gzb@clwater.top>
2024-05-04 21:16:47 +0800

    wip: fix

--- app/zy/page.tsx
+++ app/zy/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 
-
-
 import React from "react";
 import {Header} from "@/app/componets/Header";
+import {Card, Link} from "@nextui-org/react";
+import {ArrowRight} from "@/app/componets/Icons/ArrowRight";
 
 export default function ZhouYi() {
 
@@ -13,10 +13,26 @@ export default function ZhouYi() {
             className="bg-scroll  w-full items-center justify-center text-white/90 min-h-1000"
         >
             <Header routeType={2}/>
-            <p>
-                周易[......]
-            </p>
+
+            <Link className="w-full" href="/zy/gua">
+                <Card className="m-4 p-4 flex flex-row w-full">
+                    <p className="text-2xl flex-1">启卦</p>
+                    <div className="flex-none text-center ">
+                        <ArrowRight/>
+                    </div>
+                </Card>
+            </Link>
+
+            <Link className="w-full" href="/zy/zy">
+                <Card className="m-4 p-4 flex flex-row w-full">
+                    <p className="text-2xl flex-1">列表</p>
+                    <div className="flex-none text-center ">
+                        <ArrowRight/>
+                    </div>
+                </Card>
+            </Link>
 
         </div>
-    );
+    )
+        ;
 }
---
+++ app/zy/zy/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+
+
+
+import React from "react";
+import {Header} from "@/app/componets/Header";
+
+export default function ZhouYi() {
+
+    return (
+        <div
+            className="bg-scroll  w-full items-center justify-center text-white/90 min-h-1000"
+        >
+            <Header routeType={2}/>
+            <p>
+                ZY
+            </p>
+
+        </div>
+    );
+}
