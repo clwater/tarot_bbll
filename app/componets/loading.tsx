caf5e3f11b1f2d1d20263e985859782929c088bf
clwater <gzb@clwater.top>
2024-05-07 21:41:40 +0800

    wip: yao

--- app/componets/loading.tsx
+++ app/componets/loading.tsx
@@ -8,7 +8,7 @@ export default function Loading() {
 
     return (
 
-        <div className="flex min-h-10 items-center">
+        <div className="flex min-h-64 items-center">
             <div className="text-center inline-block my-auto mx-auto">
                 <CircularProgress aria-label="Loading..." className="justify-center "/>
             </div>
