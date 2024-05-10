85cff92252a2cc2ead70ecbf89c6891f596915fa
clwater <gzb@clwater.top>
2024-05-10 15:36:14 +0800

    wip: zy loading

--- app/componets/ZYLoading.tsx
+++ app/componets/ZYLoading.tsx
@@ -3,27 +3,29 @@ import React, {Suspense, useEffect} from "react";
 import {GuaImage} from "@/app/componets/zy/GuaImage";
 let RandomUtils = require('@/app/utils/RandomUtils');
 
-export default function ZYLoading() {
+function ZYLoadingItem(){
     const [yaos, setYaos] = React.useState("111111");
 
+    useEffect(() => {
+        function pollDOM() {
+            const guaImage = RandomUtils.getRandomGuas()
+            setYaos(guaImage)
+        }
 
-    // useEffect(() => {
-    //
-    //     function pollDOM() {
-    //         const guaImage = RandomUtils.getRandomGuas()
-    //         setYaos(guaImage)
-    //     }
-    //
-    //     const interval = setInterval(pollDOM, 300);
-    //
-    //     return () => clearInterval(interval);
-    // }, [])
+        const interval = setInterval(pollDOM, 1000);
+        return () => clearInterval(interval);
+    }, [yaos])
+
+    return <GuaImage images={yaos} size={1}/>
+}
+
+export default function ZYLoading() {
 
     return (
 
         <div className="flex min-h-64 items-center">
             <div className="text-center inline-block my-auto mx-auto w-1/4 min-h-32">
-                <GuaImage images={yaos} size={1}/>
+                <ZYLoadingItem/>
                 <p className="animate-bounce mt-4">筹算中</p>
             </div>
         </div>
